2e12436323211d226237393445b85d235b836197
RYA234 <mryuabc@gmail.com>
2023-04-12 07:35:13 +0900

    24 Enemyがタレットの範囲内に入った時にタレットの_enemies変数に　enemiesを追加する

--- Assets/Script/Turrets/Turrets.cs
+++ Assets/Script/Turrets/Turrets.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,42 @@ using UnityEngine;
 public class Turrets : MonoBehaviour
 {
     [SerializeField] private float attackRange = 3f;
-
+    public Enemy CurrentEnemyTarget { get; set; }
     private bool _gameStarted;
 
+    private List<Enemy> _enemies;
+    
     private void Start()
     {
         _gameStarted = true;
+        _enemies = new List<Enemy>();
     }
+
+    private void Update()
+    {
+        
+    }
+
+    private void GetCurrentEnemyTarget()
+    {
+        if (_enemies.Count <= 0)
+        {
+            CurrentEnemyTarget = null;
+            return;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Enemy newEnemy = other.GetComponent<Enemy>();
+            _enemies.Add(newEnemy);
+        }
+
+        CurrentEnemyTarget = _enemies[0];
+        
+    }
+
     private void OnDrawGizmos()
     {
         if (!_gameStarted)
