711b49fb4d12727f42e2dd1d32fa73b5c8b96b09
RYA234 <mryuabc@gmail.com>
2023-04-14 19:33:19 +0900

    シングルトン実装

--- Assets/Script/Manager/DamageTextManager.cs
+++ Assets/Script/Manager/DamageTextManager.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DamageTextManager : MonoBehaviour
+public class DamageTextManager : Singleton<DamageTextManager>
 {
     
     public ObjectPooler Pooler { get; set; }
-
-    public static DamageTextManager Instance;
-
-    private void Awake()
-    {
-        Instance = this;
-    }
+    
     // Start is called before the first frame update
     void Start()
     {
