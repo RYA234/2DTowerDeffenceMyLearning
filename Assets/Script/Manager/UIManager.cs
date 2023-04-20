8519d66b8b0e2013ba0fe3e06458e876adf963ad
RYA234 <mryuabc@gmail.com>
2023-04-20 10:59:18 +0900

    47 UpgradeのUIを実装 タレット設置済みで実施するとUpgradeUIを開くようにする

--- Assets/Script/Manager/UIManager.cs
+++ Assets/Script/Manager/UIManager.cs
@@ -1,20 +1,39 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIManager : Singleton<UIManager>
 {
+    [Header("Panels")]
     [SerializeField] private GameObject turretShopPanel;
+
+    [SerializeField] private GameObject nodeUIPanel;
+    [SerializeField] private TextMeshProUGUI upgradeText;
+    [SerializeField] private TextMeshProUGUI sellText;
     
     private Node _currentNodeSelected;
+
+    public void CloseTurretShopPanel()
+    {
+        turretShopPanel.SetActive(false);
+    }
     
+    private void ShowNodeUI()
+    {
+        nodeUIPanel.SetActive(true);
+    }
     private void NodeSelected(Node nodeSelected)
     {
         _currentNodeSelected = nodeSelected;
         if (_currentNodeSelected.IsEmpty())
         {
             turretShopPanel.SetActive(true);
+        }
+        else
+        {
+            ShowNodeUI();
         }
     }
     private void OnEnable()
