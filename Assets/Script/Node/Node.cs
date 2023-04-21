dd6d249cbb601294e4a5d6512d2b48ef934a7afd
RYA234 <mryuabc@gmail.com>
2023-04-21 19:40:09 +0900

    49 タレットの射撃範囲を実装する

--- Assets/Script/Node/Node.cs
+++ Assets/Script/Node/Node.cs
@@ -8,8 +8,18 @@ public class Node : MonoBehaviour
 {
     public static Action<Node> OnNodeSelected;
     public static Action OnTurretSold;
+    [SerializeField] private GameObject attackRangeSprite;
     
     public Turrets Turrets { get; set; }
+    
+    private float _rangeSize;
+    private Vector3 _rangeOriginalSize;
+
+    private void Start()
+    {
+        _rangeSize = attackRangeSprite.GetComponent<SpriteRenderer>().bounds.size.y;
+        _rangeOriginalSize = attackRangeSprite.transform.localScale;
+    }
 
     public void SetTurret(Turrets turrets)
     {
@@ -24,6 +34,10 @@ public class Node : MonoBehaviour
     public void SelectTurret()
     {
         OnNodeSelected?.Invoke(this);
+        if (!IsEmpty())
+        {
+            ShowTurretInfo();
+        }
     }
 
     public void SellTurret()
@@ -35,5 +49,11 @@ public class Node : MonoBehaviour
             Turrets = null;
             OnTurretSold?.Invoke();
         }
+    }
+
+    private void ShowTurretInfo()
+    {
+        attackRangeSprite.SetActive(true);
+        attackRangeSprite.transform.localScale = _rangeOriginalSize * Turrets.AttackRange / (_rangeSize / 2);
     }
 }
--- Assets/Script/Turrets/Turrets.cs
+++ Assets/Script/Turrets/Turrets.cs
@@ -8,7 +8,7 @@ public class Turrets : MonoBehaviour
     [SerializeField] private float attackRange = 3f;
     public Enemy CurrentEnemyTarget { get; set; }
     public TurretUpgrade TurretUpgrade { get; set; }
-    
+    public float AttackRange => attackRange;
     private bool _gameStarted;
 
     private List<Enemy> _enemies;
