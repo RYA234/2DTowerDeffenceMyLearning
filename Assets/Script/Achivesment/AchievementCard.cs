158b3d826c8c9c6994e01acc686b9053c8c39480
RYA234 <mryuabc@gmail.com>
2023-04-22 18:03:19 +0900

    55　Achievementsカードを追加する

---
+++ Assets/Script/Achivesment/AchievementCard.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchievementCard : MonoBehaviour
+{
+   [SerializeField] private Image achievementImage;
+   [SerializeField] private TextMeshProUGUI title;
+   [SerializeField] private TextMeshProUGUI progress;
+   [SerializeField] private TextMeshProUGUI reward;
+
+   public void SetupAchievement(Achievement achievement)
+   {
+       achievementImage.sprite = achievement.Sprite;
+       title.text = achievement.Title;
+       progress.text = achievement.ProgressToUnlock.ToString();
+       reward.text = achievement.GoldReward.ToString();
+       
+   }
+
+   void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Manager/AchievementManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievementManager : MonoBehaviour
+{
+
+    [SerializeField] private AchievementCard _achievementCardPrefab;
+    [SerializeField] private Transform achievementPanelContainer;
+    [SerializeField] private Achievement[] achievements;
+
+    private void Start()
+    {
+        LoadAchievements();
+    }
+
+    private void LoadAchievements()
+    {
+        for (int i = 0; i < achievements.Length; i++)
+        {
+            AchievementCard card = Instantiate(_achievementCardPrefab, achievementPanelContainer);
+            
+        }
+    }
+    
+}
